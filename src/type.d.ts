b44ac8e2089a1d2bb66e3bb168cfdc1b90acfd9e
yuchao.xie <yuchao.xie@alo7.com>
2020-10-14 11:00:49 +0800

    更新声明

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,4 +1,6 @@
-interface ListNode<T = number> {
+/* eslint-disable @typescript-eslint/no-unused-vars */
+class ListNode<T = number> {
     val: T
     next: ListNode<T> | null
+    constructor(val?: number, next?: ListNode | null)
 }
